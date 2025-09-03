a932d9da0546e53f823ffa2d5e84c637eef1184f
DeaRevananda <dearevananda04@gmail.com>
2025-09-03 08:42:41 -0600

    Tugas

--- bin/Tugas/HitungGajiKaryawan.dart
+++ bin/Tugas/HitungGajiKaryawan.dart
@@ -1,31 +1,26 @@
-import 'dart:io';
 void main() {
-  stdout.write("Masukkan nama karyawan: ");
-  String? nama = stdin.readLineSync();
-
-  stdout.write("Masukkan jumlah jam kerja per minggu: ");
-  int jamKerja = int.parse(stdin.readLineSync()!);
-
-  stdout.write("Masukkan upah per jam: ");
-  double upahPerJam = double.parse(stdin.readLineSync()!);
-
-  stdout.write("Apakah karyawan tetap? (y/n): ");
-  String? statusInput = stdin.readLineSync();
-  bool statusTetap = (statusInput?.toLowerCase() == "y");
+  String namaKaryawan = "Dea Revananda";
+  int jamKerja = 9;
+  double upahPerJam = 100.000;
+  bool statusTetap = true; 
 
   double gajiKotor = jamKerja * upahPerJam;
 
-  double pajak = statusTetap ? gajiKotor * 0.10 : gajiKotor * 0.05;
+  double pajak;
+  if (statusTetap) {
+    pajak = gajiKotor * 0.10; 
+  } else {
+    pajak = gajiKotor * 0.05; 
+  }
 
   double gajiBersih = gajiKotor - pajak;
 
   print("Slip Gaji Karyawan");
-  print("Nama Karyawan : $nama");
-  print("Jam Kerja     : $jamKerja jam");
-  print("Upah per Jam  : Rp$upahPerJam");
+  print("Nama Karyawan : $namaKaryawan");
   print("Status        : ${statusTetap ? "Tetap" : "Kontrak"}");
-  print("-------------------------------");
-  print("Gaji Kotor    : Rp$gajiKotor");
-  print("Pajak         : Rp$pajak");
-  print("Gaji Bersih   : Rp$gajiBersih");
+  print("Jam Kerja     : $jamKerja jam");
+  print("Upah per Jam  : Rp $upahPerJam");
+  print("Gaji Kotor    : Rp $gajiKotor");
+  print("Pajak         : Rp $pajak");
+  print("Gaji Bersih   : Rp $gajiBersih");
 }
