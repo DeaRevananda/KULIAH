ffd6ef6eb02f8ed69e1e03849580f55802d22ea5
DeaRevananda <dearevananda04@gmail.com>
2025-09-03 09:06:54 -0600

    Update Tugas

--- bin/Tugas/HitungGajiKaryawan.dart
+++ bin/Tugas/HitungGajiKaryawan.dart
@@ -1,26 +1,22 @@
 void main() {
   String namaKaryawan = "Dea Revananda";
-  int jamKerja = 9;
-  double upahPerJam = 100.000;
+  int jamKerja = 45;
+  double upahPerJam = 100000;
   bool statusTetap = true; 
 
   double gajiKotor = jamKerja * upahPerJam;
 
-  double pajak;
-  if (statusTetap) {
-    pajak = gajiKotor * 0.10; 
-  } else {
-    pajak = gajiKotor * 0.05; 
-  }
+  double pajak = gajiKotor * (statusTetap ? 0.10 : 0.05);
 
   double gajiBersih = gajiKotor - pajak;
 
   print("Slip Gaji Karyawan");
   print("Nama Karyawan : $namaKaryawan");
   print("Status        : ${statusTetap ? "Tetap" : "Kontrak"}");
-  print("Jam Kerja     : $jamKerja jam");
-  print("Upah per Jam  : Rp $upahPerJam");
-  print("Gaji Kotor    : Rp $gajiKotor");
-  print("Pajak         : Rp $pajak");
-  print("Gaji Bersih   : Rp $gajiBersih");
+  print("Jam Kerja     : $jamKerja jam/minggu");
+  print("Upah per Jam  : Rp ${upahPerJam.toStringAsFixed(0)}");
+  print("Gaji Kotor    : Rp ${gajiKotor.toStringAsFixed(0)}");
+  print("Pajak         : Rp ${pajak.toStringAsFixed(0)}");
+  print("Gaji Bersih   : Rp ${gajiBersih.toStringAsFixed(0)}");
 }
+
